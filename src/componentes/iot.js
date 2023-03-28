f660da8493dcb779731e7fe5176730dc10de62ad
Viclor27322 <vito27322@gamil.com>
2023-03-28 11:40:06 -0600

    update files

--- src/componentes/iot.js
+++ src/componentes/iot.js
@@ -6,7 +6,7 @@ class Iot extends Component{
       <div>
         <div className="iframe-container">
             <iframe
-          src="http://proyecto.heladoshuastecos.com/proyecto/tortuguero"
+          src="https://proyecto.heladoshuastecos.com/proyecto/tortuguero/"
            width="600" height="450" allowFullScreen="" loading="lazy"
           referrerPolicy="no-referrer-when-downgrade" className='w-100 p-2'></iframe>
         </div>
